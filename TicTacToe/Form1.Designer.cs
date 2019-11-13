4c21da11c88e9c2487c5a35653da8f596e32caaf
Sealia <sealiaborusiak@wp.pl>
2019-11-13 20:08:58 +0100

    Change Form Design

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -47,83 +47,128 @@
             // 
             // A00
             // 
+            this.A00.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A00.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A00.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A00.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A00.Location = new System.Drawing.Point(12, 12);
             this.A00.Name = "A00";
             this.A00.Size = new System.Drawing.Size(93, 84);
             this.A00.TabIndex = 0;
-            this.A00.UseVisualStyleBackColor = true;
+            this.A00.UseVisualStyleBackColor = false;
             this.A00.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A10
             // 
+            this.A10.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A10.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A10.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A10.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A10.Location = new System.Drawing.Point(12, 102);
             this.A10.Name = "A10";
             this.A10.Size = new System.Drawing.Size(93, 84);
             this.A10.TabIndex = 1;
-            this.A10.UseVisualStyleBackColor = true;
+            this.A10.UseVisualStyleBackColor = false;
             this.A10.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A20
             // 
+            this.A20.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A20.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A20.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A20.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A20.Location = new System.Drawing.Point(12, 192);
             this.A20.Name = "A20";
             this.A20.Size = new System.Drawing.Size(93, 84);
             this.A20.TabIndex = 2;
-            this.A20.UseVisualStyleBackColor = true;
+            this.A20.UseVisualStyleBackColor = false;
             this.A20.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A21
             // 
+            this.A21.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A21.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A21.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A21.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A21.Location = new System.Drawing.Point(111, 192);
             this.A21.Name = "A21";
             this.A21.Size = new System.Drawing.Size(93, 84);
             this.A21.TabIndex = 5;
-            this.A21.UseVisualStyleBackColor = true;
+            this.A21.UseVisualStyleBackColor = false;
             this.A21.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A11
             // 
+            this.A11.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A11.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A11.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A11.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A11.Location = new System.Drawing.Point(111, 102);
             this.A11.Name = "A11";
             this.A11.Size = new System.Drawing.Size(93, 84);
             this.A11.TabIndex = 4;
-            this.A11.UseVisualStyleBackColor = true;
+            this.A11.UseVisualStyleBackColor = false;
             this.A11.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A01
             // 
+            this.A01.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A01.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A01.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A01.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A01.Location = new System.Drawing.Point(111, 12);
             this.A01.Name = "A01";
             this.A01.Size = new System.Drawing.Size(93, 84);
             this.A01.TabIndex = 3;
-            this.A01.UseVisualStyleBackColor = true;
+            this.A01.UseVisualStyleBackColor = false;
             this.A01.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A22
             // 
+            this.A22.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A22.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A22.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A22.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A22.Location = new System.Drawing.Point(210, 192);
             this.A22.Name = "A22";
             this.A22.Size = new System.Drawing.Size(93, 84);
             this.A22.TabIndex = 8;
-            this.A22.UseVisualStyleBackColor = true;
+            this.A22.UseVisualStyleBackColor = false;
             this.A22.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A12
             // 
+            this.A12.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A12.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A12.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A12.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A12.Location = new System.Drawing.Point(210, 102);
             this.A12.Name = "A12";
             this.A12.Size = new System.Drawing.Size(93, 84);
             this.A12.TabIndex = 7;
-            this.A12.UseVisualStyleBackColor = true;
+            this.A12.UseVisualStyleBackColor = false;
             this.A12.Click += new System.EventHandler(this.ButtonClick);
             // 
             // A02
             // 
+            this.A02.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.A02.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.A02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A02.Font = new System.Drawing.Font("Georgia", 24F);
+            this.A02.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.A02.Location = new System.Drawing.Point(210, 12);
             this.A02.Name = "A02";
             this.A02.Size = new System.Drawing.Size(93, 84);
             this.A02.TabIndex = 6;
-            this.A02.UseVisualStyleBackColor = true;
+            this.A02.UseVisualStyleBackColor = false;
             this.A02.Click += new System.EventHandler(this.ButtonClick);
             // 
             // newGameButton
@@ -184,6 +229,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(493, 436);
             this.Controls.Add(this.Draws);
             this.Controls.Add(this.Owins);
@@ -200,8 +247,12 @@
             this.Controls.Add(this.A20);
             this.Controls.Add(this.A10);
             this.Controls.Add(this.A00);
+            this.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
